83aa25c139cdf786c5a854001e5d0847781d7638
JaiminRathod <jaimin.rathod@go-jek.com>
2020-08-14 17:52:15 +0530

    modified proto file

--- internal/cli/daemon/client.go
+++ internal/cli/daemon/client.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"google.golang.org/grpc"
 	"octavius/pkg/protobuf"
-
 )
 
 type Client interface {
@@ -12,29 +11,28 @@ type Client interface {
 }
 
 type client struct {
-
 }
 
 func NewClient() Client {
 	return &client{
-		}
+	}
 }
 
 func (c *client) createJob() {
-	conn, _ := grpc.Dial("localhost:8000",grpc.WithInsecure())
-	grpcClient:=protobuf.NewProcServiceClient(conn)
+	conn, _ := grpc.Dial("localhost:8000", grpc.WithInsecure())
+	grpcClient := protobuf.NewProcServiceClient(conn)
 	var args []*protobuf.Arg
 	var sec []*protobuf.Secret
-	args=append(args,&protobuf.Arg{Name: "name",Description: "name of proct"})
-	sec=append(sec,&protobuf.Secret{Name: "secret",Description: "name of secret"})
-	envVar:=&protobuf.EnvVars{Args: args,Secrets: sec}
+	args = append(args, &protobuf.Arg{Name: "name", Description: "name of proct"})
+	sec = append(sec, &protobuf.Secret{Name: "secret", Description: "name of secret"})
+	envVar := &protobuf.EnvVars{Args: args, Secrets: sec}
 
-	request:=protobuf.Metadata{EnvVars:envVar}
-	grpcClient.CreateJob(context.Background(),&request)
+	request := protobuf.Metadata{EnvVars: envVar}
+	grpcClient.CreateJob(context.Background(), &request)
 
 }
 
 func main() {
-	client:=NewClient()
+	client := NewClient()
 	client.createJob()
 }
