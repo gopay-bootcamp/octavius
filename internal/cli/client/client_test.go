9cc283e4d06f95b62101163840736264479b07a7
Debjit Chattopadhyay <debjit.chattopadhyay@go-jek.com>
2020-08-18 20:56:53 +0530

    testing grpc client

--- internal/cli/client/client_test.go
+++ internal/cli/client/client_test.go
@@ -1,1 +1,63 @@
 package client
+
+import (
+	"context"
+	"log"
+	"net"
+	"octavius/pkg/protobuf"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/test/bufconn"
+)
+
+const bufSize = 1024 * 1024
+
+var lis *bufconn.Listener
+
+func createFakeServer() {
+	lis = bufconn.Listen(bufSize)
+	s := grpc.NewServer()
+	protobuf.RegisterOctaviusServicesServer(s, &server{})
+	go func() {
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Server exited with error: %v", err)
+		}
+	}()
+}
+
+func bufDialer(context.Context, string) (net.Conn, error) {
+	return lis.Dial()
+}
+
+type server struct{}
+
+func (s *server) CreateJob(context.Context, *protobuf.RequestForMetadataPost) (*protobuf.Response, error) {
+	return &protobuf.Response{
+		Status: "success",
+	}, nil
+}
+
+func TestCreateJob(t *testing.T) {
+	createFakeServer()
+
+	ctx := context.Background()
+	conn, err := grpc.DialContext(ctx, "bufnet", grpc.WithContextDialer(bufDialer), grpc.WithInsecure())
+	if err != nil {
+		t.Fatalf("Failed to dial bufnet: %v", err)
+	}
+	client := protobuf.NewOctaviusServicesClient(conn)
+	testClient := GrpcClient{
+		client:                client,
+		connectionTimeoutSecs: 10 * time.Second,
+	}
+	testPostRequest := &protobuf.RequestForMetadataPost{}
+	res, err := testClient.CreateJob(testPostRequest)
+	testPostResponse := &protobuf.Response{
+		Status: "success",
+	}
+	assert.Nil(t, err)
+	assert.Equal(t, res, testPostResponse)
+}
