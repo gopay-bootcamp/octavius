9ae3fd386d007f90bb55afb5142640ea8dcfa066
vipinpahwa <40717158+vipinpahwa@users.noreply.github.com>
2020-08-21 20:15:05 +0530

    Change ErrorCheck() to Error()

--- internal/control_plane/server/services.go
+++ internal/control_plane/server/services.go
@@ -11,7 +11,6 @@ type octaviusServiceServer struct {
 	procExec execution.Execution
 }
 
-
 // NewProcServiceServer used to create a new execution context
 func NewProcServiceServer(exec execution.Execution) procProto.OctaviusServicesServer {
 	return &octaviusServiceServer{
@@ -21,20 +20,19 @@ func NewProcServiceServer(exec execution.Execution) procProto.OctaviusServicesSe
 
 func (s *octaviusServiceServer) PostMetadata(ctx context.Context, request *procProto.RequestToPostMetadata) (*procProto.MetadataName, error) {
 	name, err := s.procExec.SaveMetadataToDb(ctx, request.Metadata)
-	logger.ErrorCheck(err, "Posting Metadata")
+	logger.Error(err, "Posting Metadata")
 	return name, err
 }
 
 func (s *octaviusServiceServer) GetAllMetadata(ctx context.Context, request *procProto.RequestToGetAllMetadata) (*procProto.MetadataArray, error) {
 	dataList, err := s.procExec.ReadAllMetadata(ctx)
-	logger.ErrorCheck(err, "Getting Metadata")
+	logger.Error(err, "Getting Metadata")
 	return dataList, err
 }
 
-func (s *octaviusServiceServer) GetStreamLogs( request *procProto.RequestForStreamLog, server procProto.OctaviusServices_GetStreamLogsServer) (error) {
+func (s *octaviusServiceServer) GetStreamLogs(request *procProto.RequestForStreamLog, server procProto.OctaviusServices_GetStreamLogsServer) error {
 	return nil
 }
-
 
 func (s *octaviusServiceServer) ExecuteJob(ctx context.Context, execute *procProto.RequestForExecute) (*procProto.Response, error) {
 	panic("implement me")
