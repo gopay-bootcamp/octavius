8e2c4e6d7847feb2ef2ac447445c140950991e89
yashipro13 <yashipro13@gmail.com>
2020-08-25 00:53:34 +0530

    Revert "unique id for cli requests added"
    This reverts commit 19e6be28a115963bb858605151c3fdbcfa5a54b1.

--- go.mod
+++ go.mod
@@ -10,7 +10,6 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/prometheus/common v0.4.0
 	github.com/rs/zerolog v1.19.0
-	github.com/sony/sonyflake v1.0.0
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.3.0
--- internal/control_plane/server/services.go
+++ internal/control_plane/server/services.go
@@ -2,8 +2,6 @@ package server
 
 import (
 	"context"
-	"fmt"
-	uid "github.com/sony/sonyflake"
 	"octavius/internal/control_plane/logger"
 	"octavius/internal/control_plane/server/execution"
 	procProto "octavius/pkg/protobuf"
@@ -12,8 +10,6 @@ import (
 type octaviusServiceServer struct {
 	procExec execution.Execution
 }
-
-var idGenerator = uid.NewSonyflake(uid.Settings{})
 
 // NewProcServiceServer used to create a new execution context
 func NewProcServiceServer(exec execution.Execution) procProto.OctaviusServicesServer {
@@ -24,8 +20,7 @@ func NewProcServiceServer(exec execution.Execution) procProto.OctaviusServicesSe
 
 func (s *octaviusServiceServer) PostMetadata(ctx context.Context, request *procProto.RequestToPostMetadata) (*procProto.MetadataName, error) {
 	name, err := s.procExec.SaveMetadataToDb(ctx, request.Metadata)
-	uid, _ := idGenerator.NextID()
-	logger.Error(err, fmt.Sprintf("%v Job Create Request Received - Posting Metadata to etcd", uid))
+	logger.Error(err, "Job Create Request Received - Posting Metadata to etcd")
 	return name, err
 }
 
@@ -38,14 +33,11 @@ func (s *octaviusServiceServer) GetAllMetadata(ctx context.Context, request *pro
 func (s *octaviusServiceServer) GetStreamLogs(request *procProto.RequestForStreamLog, stream procProto.OctaviusServices_GetStreamLogsServer) error {
 	logString := &procProto.Log{Log: "lorem ipsum logger logger logger dumb"}
 	err := stream.Send(logString)
-	uid, _ := idGenerator.NextID()
-	logger.Error(err, fmt.Sprintf("%v GetStream Request Received - Sending stream to client", uid))
+	logger.Error(err, "GetStream Request Received - Sending stream to client")
 	return err
 }
 
 func (s *octaviusServiceServer) ExecuteJob(ctx context.Context, execute *procProto.RequestForExecute) (*procProto.Response, error) {
-	// utilize uid in logging once execute is implemented
-	// uid, _ := idGenerator.NextID()
 	logger.Fatal("Execution is yet to be implemented")
 	return nil, nil
 }
