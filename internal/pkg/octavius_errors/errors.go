18d5e16ad3a2e59d10cb8b467c771b0dc3a60749
vipinpahwa <40717158+vipinpahwa@users.noreply.github.com>
2020-08-25 13:04:10 +0530

    Change error format in octaviusErrors

--- internal/pkg/octavius_errors/errors.go
+++ internal/pkg/octavius_errors/errors.go
@@ -14,5 +14,5 @@ func New(errorCode int, err error) error {
 	return &errorStruct{errorCode, err}
 }
 func (e *errorStruct) Error() string {
-	return fmt.Sprintf("Error code=%d Becasue of %s error=%s", e.errorCode, constant.ErrorCode[e.errorCode], e.err)
+	return fmt.Sprintf("%s{error code=%d(%s)}", e.err, e.errorCode, constant.ErrorCode[e.errorCode])
 }
