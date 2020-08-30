80bfd4420b63d02a6f64c11cbdf059924e9e3270
Debjit Chattopadhyay <debjit.chattopadhyay@go-jek.com>
2020-08-30 23:18:07 +0530

    add unit test for execution

--- internal/controller/server/execution/execution_test.go
+++ internal/controller/server/execution/execution_test.go
@@ -2,78 +2,109 @@ package execution
 
 import (
 	"context"
-	"octavius/internal/controller/server/repository/metadata"
+	"errors"
+	executorRepo "octavius/internal/controller/server/repository/executor"
+	metadataRepo "octavius/internal/controller/server/repository/metadata"
+	"octavius/internal/pkg/constant"
 	"octavius/internal/pkg/log"
-	protobuf "octavius/internal/pkg/protofiles/client_cp"
-	"reflect"
+	executorCPproto "octavius/internal/pkg/protofiles/executor_cp"
+	"sync"
 	"testing"
+	"time"
+
+	"github.com/jonboulle/clockwork"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func init() {
-	log.Init("info", "")
+	log.Init("info", "", true)
 }
 
-func Test_execution_SaveMetadataToDb(t *testing.T) {
-	metadataRepoMock := new(metadata.MetadataMock)
+func TestStartExecutorHealthCheck(t *testing.T) {
+	healthChan := make(chan string)
+	sessionID := uint64(1234)
+	clock := clockwork.NewFakeClock()
+	newActiveExecutor := activeExecutor{
+		healthChan: healthChan,
+		sessionID:  sessionID,
+		timer:      clock.After(10),
+	}
 
-	metadataVal := &protobuf.Metadata{
-		Author:      "littlestar642",
-		ImageName:   "demo image",
-		Name:        "test data",
-		Description: "sample test metadata",
+	testExecutorMap := &activeExecutorMap{
+		execMap: new(sync.Map),
 	}
-	metadataResp := &protobuf.MetadataName{
-		Name: "test data",
-		Err: &protobuf.Error{
-			ErrorCode:    0,
-			ErrorMessage: "no error",
-		},
+	testExecutorMap.Put("exec 1", &newActiveExecutor)
+
+	testMetadataRepo := new(metadataRepo.MetadataMock)
+	testExecRepo := new(executorRepo.ExecutorMock)
+
+	testExecution := &execution{
+		metadataRepo:      testMetadataRepo,
+		executorRepo:      testExecRepo,
+		activeExecutorMap: testExecutorMap,
 	}
-	metadataRepoMock.On("Save", "test data", metadataVal).Return(metadataResp, nil)
-	type fields struct {
-		metadata metadata.MetadataMock
+
+	testExecRepo.On("UpdateStatus", "exec 1", "free").Return(nil)
+	testExecRepo.On("UpdateStatus", "exec 1", "expired").Return(nil)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		startExecutorHealthCheck(testExecution, testExecutorMap, "exec 1")
+		wg.Done()
+	}()
+	//Block for asserting normal condition
+	clock.BlockUntil(1)
+
+	// Advance the FakeClock forward in time
+	clock.Advance(40 * time.Second)
+
+	// Wait until the function completes
+	wg.Wait()
+
+	//assert exit condition
+	_, exists := testExecutorMap.Get("exec 1")
+	assert.Equal(t, false, exists)
+	testExecRepo.AssertExpectations(t)
+
+}
+
+func TestUpdateExecutorStatusNotRegistered(t *testing.T) {
+	testMetadataRepo := new(metadataRepo.MetadataMock)
+	testExecRepo := new(executorRepo.ExecutorMock)
+	testExec := NewExec(testMetadataRepo, testExecRepo)
+	ctx := context.Background()
+	request := executorCPproto.Ping{
+		ID:    "exec 1",
+		State: "healthy",
 	}
-	type args struct {
-		ctx      context.Context
-		metadata *protobuf.Metadata
+	testExecRepo.On("Get", "exec 1").Return(&executorCPproto.ExecutorInfo{}, errors.New(constant.NoValueFound))
+	res, err := testExec.UpdateExecutorStatus(ctx, &request)
+	testExecRepo.AssertExpectations(t)
+	assert.Nil(t, res)
+	assert.Equal(t, err, status.Error(codes.PermissionDenied, "executor not registered"))
+}
+
+func TestUpdateExecutorStatus(t *testing.T) {
+	testMetadataRepo := new(metadataRepo.MetadataMock)
+	testExecRepo := new(executorRepo.ExecutorMock)
+	testExec := NewExec(testMetadataRepo, testExecRepo)
+	ctx := context.Background()
+	request := executorCPproto.Ping{
+		ID:    "exec 1",
+		State: "free",
 	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *protobuf.MetadataName
-		wantErr bool
-	}{
-		{
-			fields: fields{
-				metadata: *metadataRepoMock,
-			},
-			args: args{
-				ctx:      context.Background(),
-				metadata: metadataVal,
-			},
-			want: &protobuf.MetadataName{
-				Name: "test data",
-				Err: &protobuf.Error{
-					ErrorCode:    0,
-					ErrorMessage: "no error",
-				},
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			e := &execution{
-				metadataRepo: tt.fields.metadata,
-			}
-			got, err := e.Save(tt.args.ctx, tt.args.metadata)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("execution.SaveMetadataToDb() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("execution.SaveMetadataToDb() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	testExecRepo.On("Get", "exec 1").Return(&executorCPproto.ExecutorInfo{}, nil)
+	testExecRepo.On("UpdateStatus", "exec 1", "free").Return(nil)
+	res, err := testExec.UpdateExecutorStatus(ctx, &request)
+	_, ok := getActiveExecutorMap(testExec.(*execution)).Get("exec 1")
+	assert.Equal(t, res.Recieved, true)
+	assert.Nil(t, err)
+	assert.Equal(t, true, ok)
+
+	res, err = testExec.UpdateExecutorStatus(ctx, &request)
+	assert.Equal(t, res.Recieved, true)
+	assert.Nil(t, err)
 }
