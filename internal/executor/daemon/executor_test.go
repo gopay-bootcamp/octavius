eaa51bebdbdf9d495bbbea71f0a081bddf73a067
Debjit Chattopadhyay <debjit.chattopadhyay@go-jek.com>
2020-09-12 10:36:43 +0530

    resolve merge issues

--- internal/executor/daemon/executor_test.go
+++ internal/executor/daemon/executor_test.go
@@ -232,67 +232,6 @@ func TestStartKubernetesService(t *testing.T) {
 	mockKubeClient.AssertExpectations(t)
 }
 
-func TestStartKubernetesService(t *testing.T) {
-	mockGrpcClient := new(client.MockGrpcClient)
-	mockKubeClient := new(kubernetes.MockKubernetesClient)
-	testClient := NewExecutorClient(mockGrpcClient)
-	testExecutorClient := testClient.(*executorClient)
-	testExecutorClient.id = "test id"
-	testExecutorClient.state = "idle"
-	testExecutorClient.kubernetesClient = mockKubeClient
-	testExecutorClient.kubeLogWaitTime = time.Second
-	testArgs := map[string]string{"data": "test data"}
-
-	testJob := &executorCPproto.Job{
-		HasJob:    true,
-		JobID:     "123",
-		ImageName: "test image",
-		JobData:   testArgs,
-	}
-
-	testExecutionContext := &executorCPproto.ExecutionContext{
-		JobK8SName: "",
-		JobID:      "123",
-		ImageName:  "test image",
-		ExecutorID: "test id",
-		Status:     "FINISHED",
-		EnvArgs:    testArgs,
-	}
-	pod := &v1.Pod{
-		TypeMeta: meta.TypeMeta{
-			Kind:       "Pod",
-			APIVersion: "v1",
-		},
-		ObjectMeta: meta.ObjectMeta{
-			Name:      "test pod",
-			Namespace: "default",
-			Labels: map[string]string{
-				"tag": "",
-				"job": "test job",
-			},
-		},
-		Status: v1.PodStatus{
-			Phase: v1.PodSucceeded,
-		},
-	}
-	stringReadCloser := ioutil.NopCloser(strings.NewReader(""))
-
-	mockGrpcClient.On("FetchJob", &executorCPproto.ExecutorID{Id: "test id"}).Return(testJob, nil)
-	mockKubeClient.On("ExecuteJob", "123", "test image", testArgs).Return("", nil)
-	mockKubeClient.On("WaitForReadyJob", "", time.Second).Return(nil)
-	mockKubeClient.On("WaitForReadyPod", "", time.Second).Return(pod, nil)
-	mockKubeClient.On("GetPodLogs", pod).Return(stringReadCloser, nil)
-	mockGrpcClient.On("SendExecutionContext", testExecutionContext).Return(&executorCPproto.Acknowledgement{}, nil)
-	mockGrpcClient.On("Ping", &executorCPproto.Ping{ID: "test id", State: "idle"}).Return(&executorCPproto.HealthResponse{Recieved: true}, nil)
-	mockGrpcClient.On("Ping", &executorCPproto.Ping{ID: "test id", State: "running"}).Return(&executorCPproto.HealthResponse{Recieved: true}, nil)
-
-	go testExecutorClient.StartKubernetesService()
-	time.Sleep(1 * time.Second)
-
-	mockGrpcClient.AssertExpectations(t)
-	mockKubeClient.AssertExpectations(t)
-}
-
 func TestStartWatch(t *testing.T) {
 	mockGrpcClient := new(client.MockGrpcClient)
 	mockKubeClient := new(kubernetes.MockKubernetesClient)
