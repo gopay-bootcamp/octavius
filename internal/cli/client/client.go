02e0afe945b4302945ed48a3e6b262aaafaa611d
vipinpahwa <40717158+vipinpahwa@users.noreply.github.com>
2020-09-17 21:54:16 +0530

    Resolve build issues in client.go

--- internal/cli/client/client.go
+++ internal/cli/client/client.go
@@ -21,7 +21,8 @@ type Client interface {
 }
 
 type GrpcClient struct {
-	client                protobuf.ClientCPServicesClient
+	metadataClient        protobuf.MetadataServicesClient
+	jobClient             protobuf.JobServicesClient
 	connectionTimeoutSecs time.Duration
 }
 
@@ -30,8 +31,10 @@ func (g *GrpcClient) ConnectClient(cpHost string) error {
 	if err != nil {
 		return status.Error(codes.Unavailable, err.Error())
 	}
-	grpcClient := protobuf.NewClientCPServicesClient(conn)
-	g.client = grpcClient
+	grpcMetadataClient := protobuf.NewMetadataServicesClient(conn)
+	grpcJobClient := protobuf.NewJobServicesClient(conn)
+	g.jobClient = grpcJobClient
+	g.metadataClient = grpcMetadataClient
 	g.connectionTimeoutSecs = time.Second
 	return nil
 }
@@ -40,7 +43,7 @@ func (g *GrpcClient) CreateMetadata(metadataPostRequest *protobuf.RequestToPostM
 	ctx, cancel := context.WithTimeout(context.Background(), g.connectionTimeoutSecs)
 	defer cancel()
 
-	res, err := g.client.PostMetadata(ctx, metadataPostRequest)
+	res, err := g.metadataClient.PostMetadata(ctx, metadataPostRequest)
 	if err != nil {
 		return nil, err
 	}
@@ -48,17 +51,17 @@ func (g *GrpcClient) CreateMetadata(metadataPostRequest *protobuf.RequestToPostM
 }
 
 func (g *GrpcClient) GetLogs(requestForLogs *protobuf.RequestForLogs) (*protobuf.Log, error) {
-	return g.client.GetLogs(context.Background(), requestForLogs)
+	return g.jobClient.GetLogs(context.Background(), requestForLogs)
 }
 
 func (g *GrpcClient) ExecuteJob(requestForExecute *protobuf.RequestForExecute) (*protobuf.Response, error) {
-	return g.client.ExecuteJob(context.Background(), requestForExecute)
+	return g.jobClient.ExecuteJob(context.Background(), requestForExecute)
 }
 
 func (g *GrpcClient) GetJobList(requestForGetJobList *protobuf.RequestForGetJobList) (*protobuf.JobList, error) {
-	return g.client.GetJobList(context.Background(), requestForGetJobList)
+	return g.jobClient.GetJobList(context.Background(), requestForGetJobList)
 }
 
 func (g *GrpcClient) DescribeJob(requestForDescribe *protobuf.RequestForDescribe) (*protobuf.Metadata, error) {
-	return g.client.DescribeJob(context.Background(), requestForDescribe)
+	return g.jobClient.DescribeJob(context.Background(), requestForDescribe)
 }
