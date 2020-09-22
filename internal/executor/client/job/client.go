f35c36ca22f44972db146c25239cf24678274bfc
vipinpahwa <40717158+vipinpahwa@users.noreply.github.com>
2020-09-22 12:57:13 +0530

    Add comments in executor/client

--- internal/executor/client/job/client.go
+++ internal/executor/client/job/client.go
@@ -10,17 +10,20 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+//Client interface declares the executor's job functions
 type Client interface {
 	ConnectClient(cpHost string, connectionTimeOut time.Duration) error
 	FetchJob(start *protofiles.ExecutorID) (*protofiles.Job, error)
 	SendExecutionContext(executionData *protofiles.ExecutionContext) (*protofiles.Acknowledgement, error)
 }
 
+//GrpcClient struct holds the data required to invoke executor's job functions
 type GrpcClient struct {
 	client                protofiles.JobServiceClient
 	connectionTimeoutSecs time.Duration
 }
 
+//ConnectClient establishes connection between JobServiceClient and controller
 func (g *GrpcClient) ConnectClient(cpHost string, connectionTimeOut time.Duration) error {
 	conn, err := grpc.Dial(cpHost, grpc.WithInsecure())
 	if err != nil {
@@ -32,12 +35,14 @@ func (g *GrpcClient) ConnectClient(cpHost string, connectionTimeOut time.Duratio
 	return nil
 }
 
+//FetchJob fetches pending job for the executor
 func (g *GrpcClient) FetchJob(request *protofiles.ExecutorID) (*protofiles.Job, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), g.connectionTimeoutSecs)
 	defer cancel()
 	return g.client.Get(ctx, request)
 }
 
+//SendExecutionContext sends the job execution data over to the controller and gets acknowledgement fromt eh controller
 func (g *GrpcClient) SendExecutionContext(executionData *protofiles.ExecutionContext) (*protofiles.Acknowledgement, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), g.connectionTimeoutSecs)
 	defer cancel()
--- internal/executor/client/registration/client_mock.go
+++ internal/executor/client/registration/client_mock.go
@@ -11,12 +11,13 @@ type MockGrpcClient struct {
 	mock.Mock
 }
 
-//Register mock executor
+//Register mock
 func (m *MockGrpcClient) Register(request *protofiles.RegisterRequest) (*protofiles.RegisterResponse, error) {
 	args := m.Called(request)
 	return args.Get(0).(*protofiles.RegisterResponse), args.Error(1)
 }
 
+//ConnectClient mock
 func (m *MockGrpcClient) ConnectClient(cpHost string, connectionTimeOut time.Duration) error {
 	args := m.Called(cpHost, connectionTimeOut)
 	return args.Error(0)
