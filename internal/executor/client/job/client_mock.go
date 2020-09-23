8c844b89b9dd367d5f307fa0b22c0612ef7c97fd
Vipin Pahwa <40717158+vipinpahwa@users.noreply.github.com>
2020-09-23 12:07:27 +0530

    Adding tests in executor/client (#96)
    * Add tests in executor/client
    
    * Return nil as repsonse from Execute and Logs
    
    * Add comments in executor/client
    
    * Delete daemon_test.go
    
    * Delete daemon.go
    
    * Delete daemon_test.go
    
    * Delete daemon_test.go
    
    Co-authored-by: avinash <avinashjaiswal642@gmail.com>

--- internal/executor/client/job/client_mock.go
+++ internal/executor/client/job/client_mock.go
@@ -11,16 +11,19 @@ type MockGrpcClient struct {
 	mock.Mock
 }
 
+//ConnectClient mock
 func (m *MockGrpcClient) ConnectClient(cpHost string, connectionTimeOut time.Duration) error {
 	args := m.Called(cpHost, connectionTimeOut)
 	return args.Error(0)
 }
 
+//FetchJob mock
 func (m *MockGrpcClient) FetchJob(start *protofiles.ExecutorID) (*protofiles.Job, error) {
 	args := m.Called(start)
 	return args.Get(0).(*protofiles.Job), args.Error(1)
 }
 
+//SendExecutionContext mock
 func (m *MockGrpcClient) SendExecutionContext(executionData *protofiles.ExecutionContext) (*protofiles.Acknowledgement, error) {
 	args := m.Called(executionData)
 	return args.Get(0).(*protofiles.Acknowledgement), args.Error(1)
